02f7a9a95f95c4662f5e98ce210097fd30de721b
Your Name <marinanasser8@gmail.com>
2022-12-28 13:37:12 +0200

    deleted unwanted files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect,jsonify
-
+import fun
 
 app = Flask(__name__)
 @app.route('/')
--- functions.py
+++ functions.py
@@ -3,17 +3,6 @@ from PIL import image #pip install Pillow
 import matplotlib.pyplot as plt
 import random
 
-def crop(image_path, coords, saved_location):
-    """
-    @param image_path: the path to the image to edit
-    @param coords: a tuple of x/y coordinates (x1, y1, x2, y2)
-    @param saved_location: path to save the cropped image
-    """
-    image_obj = image.open(image_path)
-    cropped_image = image_obj.crop(coords)
-    cropped_image.save(saved_location)
-    cropped_image.show()
-
 def fourier_transform_shift(img):
     img_fft= np.fft.fftshift(np.fft.fft2(img))
     img_magnitude = np.sqrt(np.real(img_fft) ** 2 + np.imag(img_fft) ** 2)
