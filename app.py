b064a626859b049caa05b8291d4d308f6f9de4da
Your Name <marinanasser8@gmail.com>
2022-12-28 14:01:56 +0200

    removing forward codes

--- app.py
+++ app.py
@@ -11,31 +11,23 @@ def home():
 
 @app.route("/mag1/", methods=['POST'])
 def magnitude1():
-    #Moving forward code
-    forward_message = "Moving Forward..."
     print("mag1")
-    return render_template('main.html', forward_message=forward_message);
+    return render_template('main.html');
 
 @app.route("/phase1/", methods=['POST'])
 def phase1():
-    #Moving forward code
-    forward_message = "Moving Forward..."
     print("phase1")
-    return render_template('main.html', forward_message=forward_message);
+    return render_template('main.html');
 
 @app.route("/mag2/", methods=['POST'])
 def magnutude2():
-    #Moving forward code
-    forward_message = "Moving Forward..."
     print("mag2")
-    return render_template('main.html', forward_message=forward_message);
+    return render_template('main.html');
 
 @app.route("/phase2/", methods=['POST'])
 def phase2():
-    #Moving forward code
-    forward_message = "Moving Forward..."
     print("phase2")
-    return render_template('main.html', forward_message=forward_message);
+    return render_template('main.html');
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
