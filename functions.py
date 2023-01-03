ff8386540ed72073558700b60050714d62b9f902
joeadham <yousefdhm@gmail.com>
2023-01-03 21:45:23 +0200

    Commented unneeded functions

--- functions.py
+++ functions.py
@@ -66,18 +66,7 @@ class Image:
         # masked = np.fft.ifftshift(masked)
         print('masked')
         return masked
-    def combine(fourierToMag,fourierToPhase):
-        combined = np.multiply(np.abs(fourierToMag), np.exp(1j*np.angle(fourierToPhase)))
-        newInSpatial = np.fft.ifft2(combined)
-        img_back = np.real(np.fft.ifft2(newInSpatial))
-        img_back = np.abs(newInSpatial)
-        
-        name = f'result{random.randint(1,10000)}.jpg'
-        path = f'static/assets/{name}'
-        cv2.imwrite(path,img_back)
-        print('combined')
-        print(path)
-        return path
+   
     def getPathOfMagOrPhasePlot(self,magTPhaseF):
         # if 1 get mag if 0 get phase
         if magTPhaseF:
@@ -110,35 +99,47 @@ class Image:
  
     
     
-#not needed any more    
-def fourier_transform_shift(img):
-    img_fft= np.fft.fftshift(np.fft.fft2(img))
-    img_magnitude = np.sqrt(np.real(img_fft) ** 2 + np.imag(img_fft) ** 2)
-    img_phase = np.arctan2(np.imag(img_fft), np.real(img_fft))
-    return img_fft,img_magnitude,img_phase
+# #not needed any more    
+# def fourier_transform_shift(img):
+#     img_fft= np.fft.fftshift(np.fft.fft2(img))
+#     img_magnitude = np.sqrt(np.real(img_fft) ** 2 + np.imag(img_fft) ** 2)
+#     img_phase = np.arctan2(np.imag(img_fft), np.real(img_fft))
+#     return img_fft,img_magnitude,img_phase
 
-def reconstruct_image(mag,phase):
-    img_comb = np.multiply(mag, np.exp(1j * phase))
-    resulting_img= np.real(np.fft.ifft2(np.fft.ifftshift(img_comb)))
-    return resulting_img
+# def reconstruct_image(mag,phase):
+#     img_comb = np.multiply(mag, np.exp(1j * phase))
+#     resulting_img= np.real(np.fft.ifft2(np.fft.ifftshift(img_comb)))
+#     return resulting_img
 
-def plot_magnitude(img_magnitude):
+# def plot_magnitude(img_magnitude):
     
     
-    # plt.figure(figsize=[15, 8])
-    # plt.imshow(np.log(img_magnitude+1e-10), cmap='gray')
-    name = f'magnitude{random.randint(1,10000)}.jpg'
-    path = f'static/assets/{name}'
-    cv2.imwrite(path,20*np.log(np.abs(img_magnitude)))
-    # plt.savefig(path)
-    # plt.close()
-    return name
+#     # plt.figure(figsize=[15, 8])
+#     # plt.imshow(np.log(img_magnitude+1e-10), cmap='gray')
+#     name = f'magnitude{random.randint(1,10000)}.jpg'
+#     path = f'static/assets/{name}'
+#     cv2.imwrite(path,20*np.log(np.abs(img_magnitude)))
+#     # plt.savefig(path)
+#     # plt.close()
+#     return name
 
-def plot_phase(img_phase):
-    plt.figure(figsize=[8, 4])
-    plt.imshow((img_phase), cmap='gray')
-    path = f'static/img/phase{random.randint(1,10000)}.jpg'
-    plt.savefig(path)
-    plt.close()
-    return path
+# def plot_phase(img_phase):
+#     plt.figure(figsize=[8, 4])
+#     plt.imshow((img_phase), cmap='gray')
+#     path = f'static/img/phase{random.randint(1,10000)}.jpg'
+#     plt.savefig(path)
+#     plt.close()
+#     return path
 
+#  def combine(fourierToMag,fourierToPhase):
+#         combined = np.multiply(np.abs(fourierToMag), np.exp(1j*np.angle(fourierToPhase)))
+#         newInSpatial = np.fft.ifft2(combined)
+#         img_back = np.real(np.fft.ifft2(newInSpatial))
+#         img_back = np.abs(newInSpatial)
+        
+#         name = f'result{random.randint(1,10000)}.jpg'
+#         path = f'static/assets/{name}'
+#         cv2.imwrite(path,img_back)
+#         print('combined')
+#         print(path)
+#         return path
